4656a8d2a3bf449db743dba2ec50a9fffc393f32
yenilikci <m.melihcelik017@outlook.com>
2023-01-22 02:45:49 +0300

    Set ground component

---
+++ src/components/Ground/Ground.jsx
@@ -0,0 +1,29 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+
+const Ground = () => {
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+      <planeGeometry args={[30, 30]} />
+      <MeshReflectorMaterial
+        envMapIntensity={0}
+        dithering={true}
+        color={[0.015, 0.015, 0.015]}
+        roughness={0.7}
+        blur={[1000, 400]}
+        mixBlur={30}
+        mixStrength={80}
+        mixContrast={1}
+        resolution={1024}
+        mirror={0}
+        depthScale={0.01}
+        minDepthThreshold={0.9}
+        maxDepthThreshold={1}
+        depthToBlurRatioBias={0.25}
+        debug={0}
+        reflectorOffset={0.2}
+      />
+    </mesh>
+  );
+};
+
+export default Ground;
