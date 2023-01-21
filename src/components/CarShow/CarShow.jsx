dd06a377f731d2e75f0c6cdaf9fafd103511380b
yenilikci <m.melihcelik017@outlook.com>
2023-01-22 02:39:31 +0300

    Added background and spotlights

--- src/components/CarShow/CarShow.jsx
+++ src/components/CarShow/CarShow.jsx
@@ -5,10 +5,25 @@ const CarShow = () => {
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"red"} />
-      </mesh>
+      <color args={[0, 0, 0]} attach="background" />
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
+        <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
     </>
   );
 };
